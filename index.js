55623ca551eb86ac0b370bd0d4a330c3e276fc41
Kane2881 <kanethepro2881@gmail.com>
2024-02-12 16:38:19 +0630

    Initial commit

---
+++ index.js
@@ -0,0 +1,112 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let passwordEl1 = document.getElementById("passowrd1-el");
+let passwordEl2 = document.getElementById("passowrd2-el");
+let passwordLength = 12;
+function generateRandomCharacter() {
+  let randomCharacter = Math.floor(Math.random() * characters.length);
+  return characters[randomCharacter];
+}
+
+function generateRandomPassword() {
+  let randomPassword1 = "";
+  let randomPassword2 = "";
+  for (let i = 0; i < passwordLength; i++) {
+    randomPassword1 += generateRandomCharacter();
+    randomPassword2 += generateRandomCharacter();
+    passwordEl1.textContent = randomPassword1;
+    passwordEl2.textContent = randomPassword2;
+  }
+}
